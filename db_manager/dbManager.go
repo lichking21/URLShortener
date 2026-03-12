c59bc1b76a16820f25e496d0b6d4583f3c1f9070
lichking21 <kazakovaman@gmail.com>
2026-03-12 21:46:56 +0100

    (UPD) code refactoring

--- db_manager/dbManager.go
+++ db_manager/dbManager.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
-func ConnectToDb() (*pgx.Conn, error) {
+func GetConnection() (*pgx.Conn, error) {
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatalln("(FATAL) >> .env file wasn't found")
@@ -26,4 +27,26 @@ func ConnectToDb() (*pgx.Conn, error) {
 	}
 
 	return conn, err
+}
+
+func ConnectToDb() *pgx.Conn {
+
+	conn, err := GetConnection()
+	if err != nil {
+		log.Fatalf("(FATAL) >> Не удалось установить соединение с БД: %v", err)
+		return nil
+	}
+
+	defer conn.Close(context.Background())
+
+	pingCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := conn.Ping(pingCtx); err != nil {
+		log.Fatalf("(FATAL) >> База данных не отвечает на Ping: %v", err)
+		return nil
+	}
+
+	log.Println("(LOG) >> Successfully connected to db")
+	return conn
 }
--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"context"
-	"log"
-	"time"
 	dbmanager "urlShortener/db_manager"
+	redismanager "urlShortener/redis_manager"
 )
 
 func main() {
 
-	conn, err := dbmanager.ConnectToDb()
-	if err != nil {
-		log.Fatalf("(FATAL) >> Не удалось установить соединение с БД: %v", err)
-	}
-
-	defer conn.Close(context.Background())
-
-	pingCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := conn.Ping(pingCtx); err != nil {
-		log.Fatalf("(FATAL) >> База данных не отвечает на Ping: %v", err)
-	}
-
-	log.Println("(LOG) >> Successfully connected to db")
+	dbmanager.ConnectToDb()
+	redismanager.CreateClient()
 }
