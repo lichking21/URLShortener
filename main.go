40833e8f06856805cd4fcc030e972319239e7fae
lichking21 <kazakovaman@gmail.com>
2026-03-13 21:28:07 +0100

    main

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"context"
+	"log"
+	"urlShortener/base62"
 	dbmanager "urlShortener/db_manager"
-	redismanager "urlShortener/redis_manager"
+	"urlShortener/storage"
 )
 
 func main() {
 
-	dbmanager.ConnectToDb()
-	redismanager.CreateClient()
+	dbPool, err := dbmanager.ConnectToDb()
+	if err != nil {
+		log.Fatalln("(FATAL) >> Failed to connect to db: ", err)
+	}
+
+	ctx := context.Background()
+	// redismanager.CreateClient()
+
+	urlString := "https://github.com/lichking21"
+
+	repo := storage.NewPostgresRepo(dbPool)
+
+	id, _ := repo.AddOrigUrl(ctx, urlString)
+	shortCode := base62.Encode(id)
+	repo.UpdateShortCode(ctx, id, shortCode)
+
+	url, _ := repo.GetByShortCode(ctx, shortCode)
+
+	storage.PrintUrl(url)
 }
