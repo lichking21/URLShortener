26089800b484843c7b52210c54db8303584e2562
lichking21 <kazakovaman@gmail.com>
2026-03-14 10:53:55 +0100

    (NEW) functions to write/read data to/from cache

--- redis_manager/redis_manager.go
+++ redis_manager/redis_manager.go
@@ -1,24 +1,24 @@
 package redismanager
 
 import (
+	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
-func CreateClient() *redis.Client {
+func CreateClient() (*redis.Client, error) {
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatalln("(FATAL) >> .env file wasn't found")
-		return nil
 	}
 
 	redisUrl := os.Getenv("REDIS_URL")
 	if redisUrl == "" {
 		log.Fatalln("(FATAL) >> redisUrl is empty")
-		return nil
 	}
 
 	options, err := redis.ParseURL(redisUrl)
@@ -29,5 +29,25 @@ func CreateClient() *redis.Client {
 	client := redis.NewClient(options)
 	log.Println("(LOG) >> Client was successfully created!")
 
-	return client
+	return client, nil
+}
+
+func ReadFromCache(ctx context.Context, shortCode string, client *redis.Client) (string, error) {
+
+	data, err := client.Get(ctx, shortCode).Result()
+
+	if err != nil {
+		return "", err
+	}
+
+	return data, nil
+}
+
+func WriteToCache(ctx context.Context, shortCode string, longUrl string, client *redis.Client) error {
+
+	if err := client.Set(ctx, shortCode, longUrl, 24*time.Hour).Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
